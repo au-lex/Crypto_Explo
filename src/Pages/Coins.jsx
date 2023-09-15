dc03b84688a24d878edffc15c95000c091582c13
au-lex <aulexc4d2@gmail.com>
2023-09-15 10:58:07 +0100

    features ui component done

---
+++ src/Pages/Coins.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeaderFooter from '../Component/Header/HeaderFooter'
+
+const Coins = () => {
+    return (
+        <div className='text-white'>
+            coins
+
+
+
+<HeaderFooter />
+        </div>
+    )
+}
+
+export default Coins
